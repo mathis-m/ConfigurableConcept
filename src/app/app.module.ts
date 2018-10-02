2391fd045ee3b56ab383a9664d5f309cf1cb856e
Mathis Michel <Mathis_Michel@hugoboss.com>
2018-10-02 14:19:36 +0200

    imports and change standart app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {RouterModule, Routes} from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { CCPageComponent } from './shared/components/ccpage/ccpage.component';
+import { CCElementComponent } from './shared/components/ccelement/ccelement.component';
+
+const routes: Routes = [
+  { path: '**', component: CCPageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CCPageComponent,
+    CCElementComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
